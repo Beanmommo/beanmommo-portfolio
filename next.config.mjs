aaec3fb3b0d5f4eb90c866a1cf1ce04a54f7b16e
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2024-04-22 22:25:19 +1000

    chore: change deployment to vercel instead of github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = (process.env.NODE_ENV = "production");
 
-const nextConfig = {
-  basePath: isProd ? "/beanmommo-portfolio" : "",
-  output: "export", //enables static export
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

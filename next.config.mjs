fa2ef289d3f0bda8cb87e49f0be2c17917b03285
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2024-04-22 15:08:15 +1000

    chore: changed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = (process.env.NODE_ENV = "production");
+
 const nextConfig = {
+  basePath: isProd ? "/beanmommo-portfolio" : "",
   output: "export", //enables static export
   distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
